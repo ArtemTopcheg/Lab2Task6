2af59a8682a10f71c8867df8ac4808f8100cbeef
Артём Шустов < @DESKTOP-3G11VV9>
2022-09-26 21:36:39 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿//Task6
+
+int[] nums = new int[100];
+int count = 0;
+Console.WriteLine("Введите массив из 10 значений");
+for (int i = 0; i < 10; i++)
+{
+    int iElement = Convert.ToInt32(Console.ReadLine());
+    nums[i] = iElement;
+
+    if (iElement < 0)
+    {
+        nums[i] = iElement;
+        nums[i + 1] = iElement;
+        i++;
+        count += 2;
+    }
+    else
+    {
+        nums[i] = iElement;
+        count++;
+    }
+}
+Console.WriteLine("Итоговый массив");
+for (int i = 0; i < count; i++)
+{
+    Console.Write(nums[i] + " ");
+}
